c769789cd2829a5d07ef1eabaeda3e49d0a24e0e
ronexlemon <ronexondimu@gmail.com>
2026-02-10 10:46:55 +0300

    ft:swap

--- DSA/memory/pointer.go
+++ DSA/memory/pointer.go
@@ -33,9 +33,33 @@ func Pointer(){
 
 	p.Name = "John"
 	println("The Struct person",p.Name)
+	CheckSwap()
 }
 
 
 func UpdateName(name *string){
 	*name = "Yes Baanah"
+}
+
+
+func Swap(a,b *int){
+	placeHolder := *a
+
+	*a = *b 
+
+	*b = placeHolder
+
+
+}
+func SwapB (a,b *int){
+	*a,*b =*b,*a
+}
+
+func CheckSwap(){
+	num1 := 10
+	num2:=20
+	//Swap(&num1,&num2)
+	SwapB(&num1,&num2)
+	fmt.Printf("The value of num1 :%d and num2 : %d",num1,num2)
+
 }
